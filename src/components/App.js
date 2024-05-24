12e013fde429c9a6667fc8a1ce791afcd61321e6
ahmadqaranyfarhan <ahmadqaranyfarhan5@gmail.com>
2024-05-24 16:34:35 +0300

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Nav from "./Nav";
-
+import Hogs from "./Hogs.";
 import hogs from "../porkers_data";
 
 function App() {
-	return (
-		<div className="App">
-			<Nav />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Nav />
+      <Hogs hogs={hogs} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,85 @@
-import React from "react";
-import piggy from "../assets/porco.png";
+import React, { useState } from "react";
 
-const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
-	);
-};
+function Hogs({ hogs }) {
+  const [showMoreMap, setShowMoreMap] = useState({});
+  const [filterGreased, setFilterGreased] = useState(false);
+  const [sortHogsTile, setSortHogsTile] = useState(null);
 
-export default Nav;
+  function toggleButton(id) {
+    setShowMoreMap((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+  }
+
+  function filterHogs(hogs) {
+    if (!filterGreased) {
+      return hogs;
+    }
+    return hogs.filter((hog) => hog.greased);
+  }
+
+  function sortHogs(hogs) {
+    if (!sortHogsTile) {
+      return hogs;
+    }
+    return [...hogs].sort((a, b) => {
+      if (sortHogsTile === "name") {
+        return a.name.localeCompare(b.name);
+      } else if (sortHogsTile === "weight") {
+        return a.weight - b.weight;
+      }
+      return 0;
+    });
+  }
+
+  const sortedAndFilteredHogs = sortHogs(filterHogs(hogs));
+
+  return (
+    <div className="hogsContainer">
+      <div className="tile">
+        <button onClick={() => setFilterGreased(!filterGreased)}>
+          {filterGreased ? "Show All" : "Filter Greased"}
+        </button>
+        <h1>Hogs</h1>
+        <select
+          value={sortHogsTile || ""}
+          onChange={(e) => setSortHogsTile(e.target.value || null)}
+        >
+          <option value="">Sort By</option>
+          <option value="name">Name</option>
+          <option value="weight">Weight</option>
+        </select>
+      </div>
+
+      {sortedAndFilteredHogs.map((hog, id) => (
+        <div key={id} className="ui card">
+          <a className="image">
+            <img src={hog.image} alt={hog.name} />
+          </a>
+          <div className="content">
+            <a onClick={() => toggleButton(id)} className="header">
+              {hog.name}
+            </a>
+            {showMoreMap[id] && (
+              <div>
+                <p>
+                  Weight: <span>{hog.weight}</span>
+                </p>
+                <p>
+                  Greased: <span>{hog.greased ? "Yes" : "No"}</span>
+                </p>
+                <p>
+                  Highest Medal Achieved:{" "}
+                  <span>{hog["highest medal achieved"]}</span>
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Hogs;
